c10f40bed90462b2ee11a58758755a64bf893cd9
unknown <greateducation123@yahoo.com>
2023-01-09 11:50:16 -0600

    Created a PUT endpoint that accepts a value from the requests URL + finds product in product table and updates that product with properties that were sent in the requests body + returned a 200 status code + responded with the newly updated product object

--- products/views.py
+++ products/views.py
@@ -20,11 +20,14 @@ def products_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])       
+@api_view(['GET', 'PUT'])       
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
-    serializer = ProductSerializer(product);
-    return Response(serializer.data)
-    
-
-    
+    if request.method == 'GET':       
+        serializer = ProductSerializer(product);
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+         serializer = ProductSerializer(product, data=request.data)     
+         serializer.is_valid(raise_exception=True)  
+         serializer.save()
+         return Response(serializer.data)

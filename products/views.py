9515aff950c28c07011e54506f1da138dd0bfaad
unknown <greateducation123@yahoo.com>
2023-01-09 10:20:15 -0600

    Created a POST endpoint that accepts a body object request in the form of a Product model + added new product to database + returns a 201 status code + respons with newly created product object

--- products/views.py
+++ products/views.py
@@ -3,11 +3,19 @@ from rest_framework.response import Response
 from .serializers import ProductSerializer
 from .models import Product
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def products_list(request):
-    
-    products = Product.objects.all()
 
-    serializer = ProductSerializer(products, many=True)
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
 
-    return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data, status =201)
+        else:
+            return Response(serializer.errors, status=400)
